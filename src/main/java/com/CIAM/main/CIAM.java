8026dec5631a9c723708b09223f2dc65c04802b1
jojos <jojos@JonasspeedLaptop.fritz.box>
2016-10-25 20:41:17 +0200

    Basis CIAM

---
+++ src/main/java/com/CIAM/main/CIAM.java
@@ -0,0 +1,19 @@
+package com.CIAM.main;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = CIAM.MODID, version = CIAM.VERSION)
+public class CIAM
+{
+    public static final String MODID = "CIAM";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+}
