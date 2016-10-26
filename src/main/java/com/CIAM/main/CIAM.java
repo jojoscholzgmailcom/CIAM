f2fa5420e2371f1d07401ece6def6d90338db3e1
jojos <jojos@JonasspeedLaptop.fritz.box>
2016-10-26 15:08:41 +0200

    Erweiterte Basis

--- src/main/java/com/CIAM/main/CIAM.java
+++ src/main/java/com/CIAM/main/CIAM.java
@@ -4,15 +4,28 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = CIAM.MODID, version = CIAM.VERSION)
 public class CIAM
 {
     public static final String MODID = "CIAM";
     public static final String VERSION = "1.0";
+    @EventHandler
+    public void preinit(FMLPreInitializationEvent preevent)
+    {
+		
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
+    {
+		
+    }
+    
+    @EventHandler
+    public void postinit(FMLPostInitializationEvent postevent)
     {
 		
     }
