4ae7b66ea924e8d174c2d7ca73719923168acebe
jojos <jojos@JonasspeedLaptop.fritz.box>
2016-10-28 19:38:44 +0200

    verbesserungen Item

---
+++ src/main/java/com/ciam/items/dienstmarke.java
@@ -0,0 +1,12 @@
+package com.ciam.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class dienstmarke extends Item{
+	
+	public dienstmarke() {
+		this.setUnlocalizedName("dienstmarke");
+		this.setCreativeTab(CreativeTabs.tabAllSearch);
+	}
+}
