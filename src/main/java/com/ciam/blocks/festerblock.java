c77df426f3feac95c52fbff8cca511bcbb241669
jojos <jojos@JonasspeedLaptop.fritz.box>
2016-10-28 16:10:16 +0200

    Easteregg hinzugefügt

--- src/main/java/com/ciam/blocks/festerblock.java
+++ src/main/java/com/ciam/blocks/festerblock.java
@@ -2,7 +2,13 @@ package com.ciam.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
+import net.minecraft.world.World;
 
 public class festerblock extends Block{
 
@@ -14,5 +20,12 @@ public class festerblock extends Block{
 		//this.setBlockUnbreakable();
 		this.setHardness(10000F);
 	}
-
+	
+	@Override
+	public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state) {
+		if(!worldIn.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), -1).capabilities.isCreativeMode){
+		worldIn.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), -1).addChatComponentMessage(new ChatComponentText(EnumChatFormatting.GOLD + "Wow, du hast echt lange durch gehalten."));
+		}
+		super.onBlockDestroyedByPlayer(worldIn, pos, state);
+	}
 }
