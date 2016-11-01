a031f13ee0328750db4c6e97b612872d2fe99bb0
jojos <jojos@JonasspeedLaptop.fritz.box>
2016-11-01 11:45:38 +0100

    Änderungen der Entities

--- src/main/java/com/ciam/entity/Entitydieb.java
+++ src/main/java/com/ciam/entity/Entitydieb.java
@@ -4,11 +4,13 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIBreakDoor;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
 import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAIOpenDoor;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
@@ -28,10 +30,15 @@ public class Entitydieb extends EntityMob{
 		this.tasks.addTask(0, new EntityAISwimming(this));
         this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
         this.tasks.addTask(3, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(2, this.field_175455_a);
         this.tasks.addTask(4, new EntityAIWander(this, 1.0D));
-        this.tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(5, new EntityAIWatchClosest(this, Entitywache.class, 8.0F));
+        this.tasks.addTask(15, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(14, new EntityAIWatchClosest(this, EntityVillager.class, 8.0F));
+        this.tasks.addTask(16, new EntityAIOpenDoor(this, true));
         this.tasks.addTask(6, new EntityAILookIdle(this));
         this.tasks.addTask(7, new EntityAIAttackOnCollide(this, EntityIronGolem.class, 1.0D, true));
+        this.tasks.addTask(13, new EntityAIAttackOnCollide(this, Entitywache.class, 1.0D, true));
         this.tasks.addTask(8, new EntityAIMoveThroughVillage(this, 1.0D, false));
         this.targetTasks.addTask(9, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
         this.targetTasks.addTask(10, new EntityAINearestAttackableTarget(this, EntityVillager.class, true));
@@ -42,11 +49,13 @@ public class Entitydieb extends EntityMob{
 
 	@Override
 	protected void applyEntityAttributes() {
-		getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(3);
+		super.applyEntityAttributes();
+		getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(0.1D);
 		getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(20);
 		getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(45);
 		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.33000000417232513D);
-		getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(0);
+		getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(3);
+		
 		
 	}
 	
